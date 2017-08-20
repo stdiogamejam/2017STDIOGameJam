24bebdeb43f0eda170fa497bf842afaee33bef3b
Racso <correo@oscargomez.net>
2017-08-20 15:48:18 -0500

    Refactoring: changing the code from using a closure to using OOP. Adding board sizes and difficulties.

--- racso/rpgio/rpgio.py
+++ racso/rpgio/rpgio.py
@@ -7,81 +7,75 @@
 
 from random import randint as rand
 
-GOBLIN, POTION, MONEY, TRAP, EMPTY = "Goblin", "Potion", " Gold ", " Trap ", "      "
+class Game():
 
-def Game():
-    global GOBLIN, POTION, MONEY, TRAP, EMPTY
-    def printBoard():
-        nonlocal mode
-        for r in range(size):
-            if mode==1:
-                print(" ".join(["/ "+item+" \\" for item in board[r]]))
-                print(" ".join(["\\ "+str(num).center(6)+" /" for num in range(r*size, (r+1)*size)]))
-            else:
-                print("".join([item for item in board[r]]) + "   " + str(r*size)+"-"+str((r+1)*size-1))
+    def xy(self,cell):
+        return cell % self.size, cell//self.size
 
-    def printStats():
-        nonlocal hp, gold, turn
-        print("Current round: " + str(turn))
-        print("Your HP: " + str(hp))
-        print("Your gold: " + str(gold))
+    def cell(self,x,y):
+        return y*self.size+x
     
-    def xy(cell):
-        return cell % size, cell//size
+    def printBoard(self):
+        for r in range(self.size):
+            if self.mode==1:
+                print(" ".join(["/ "+item+" \\" for item in self.board[r]]))
+                print(" ".join(["\\ "+str(num).center(6)+" /" for num in range(r*self.size, (r+1)*self.size)]))
+            else:
+                print("".join([item for item in self.board[r]]) + "   " + str(r*self.size)+"-"+str((r+1)*self.size-1))
 
-    def cell(x,y):
-        return y*size+x
-        
-    def takenCells(initial):
-        initialPos = xy(initial)
+    def printStats(self):
+        print("Current round: " + str(self.turn))
+        print("Your HP: " + str(self.hp))
+        print("Your Gold: " + str(self.gold))
+            
+    def takenCells(self,initial):
+        initialPos = self.xy(initial)
         taken = { initialPos }
         for direction in [(1,0),(-1,0),(0,1),(0,-1)]:
-            for dif in range(1, size):
+            for dif in range(1, self.size):
                 pos = (initialPos[0]+dif*direction[0], initialPos[1]+dif*direction[1])
-                if not(0<=pos[0]<size and 0<=pos[1]<size) or board[pos[1]][pos[0]]!=board[initialPos[1]][initialPos[0]]: break
+                if not(0<=pos[0]<self.size and 0<=pos[1]<self.size) or self.board[pos[1]][pos[0]]!=self.board[initialPos[1]][initialPos[0]]: break
                 taken.add(pos)
         return taken
 
-    def processCells(cells):
-        nonlocal hp, gold
-        element = board[cells[0][1]][cells[0][0]]
+    def processCells(self,cells):
+        element = self.board[cells[0][1]][cells[0][0]]
         amount = len(cells)
-        if element==GOBLIN:
+        if element==self.GOBLIN:
             print("You killed %s goblins!" % amount)
-        elif element==POTION:
+        elif element==self.POTION:
             print("You drank %s potions!" % amount)
-            hp += amount
-        elif element==MONEY:
+            self.hp += amount
+        elif element==self.MONEY:
             print("You picked %s gold!" % amount)
-            gold += amount
-        elif element==TRAP:
+            self.gold += amount
+        elif element==self.TRAP:
             print("You disabled %s traps." % amount)
 
-    def removeFromBoard(cells):
+    def removeFromBoard(self,cells):
         for cell in cells:
-            board[cell[1]][cell[0]]=EMPTY
+            self.board[cell[1]][cell[0]]=self.EMPTY
         while True:
             changeMade = False
-            for row in range(size-1, 0, -1):                
-                for col in range(size):
-                    if board[row][col]==EMPTY and board[row-1][col]!=EMPTY:
-                        changeMade, board[row-1][col], board[row][col] = True, EMPTY, board[row-1][col]
+            for row in range(self.size-1, 0, -1):                
+                for col in range(self.size):
+                    if self.board[row][col]==self.EMPTY and self.board[row-1][col]!=self.EMPTY:
+                        changeMade, self.board[row-1][col], self.board[row][col] = True, self.EMPTY, self.board[row-1][col]
             if changeMade == False: break
         return
 
-    def receiveDamage():
-        nonlocal hp
-        goblins = sum([row.count(GOBLIN) for row in board])
+    def receiveDamage(self):
+        goblins = sum([row.count(self.GOBLIN) for row in self.board])
         print("You are attacked by %s goblins! Each one does 1 damage!" % goblins)
-        hp -= goblins
+        self.hp -= goblins
 
-    def fillBoard():
-        for r in range(size):
-            for c in range(size):
-                if board[r][c]==EMPTY: board[r][c] = items[rand(0,len(items)-1)]
+    def fillBoard(self):
+        for r in range(self.size):
+            for c in range(self.size):
+                if self.board[r][c]== self.EMPTY: self.board[r][c] = self.items[rand(0,len(self.items)-1)]
 
 
-    def read(prompt, error, minimum, maximum):
+    def read(self, prompt, error, minimum, maximum):
         while True:
             try:
                 command = int(input(prompt))
@@ -89,64 +83,70 @@ def Game():
                 return command
             except:
                 print(error)
+
+    def run(self):
+        print(
+     '''
+      _____    _____     _____       _       
+     |  __ \  |  __ \   / ____|     (_)      
+     | |__) | | |__) | | |  __       _  ___  
+     |  _  /  |  ___/  | | |_ |     | |/ _ \ 
+     | | \ \  | |      | |__| |  _  | | (_) |
+     |_|  \_\ |_|       \_____| (_) |_|\___/
+     By RACSO. http://racso.co\n\n
+     ''')
+        print("You're exploring a dangerous dungeon to collect GOLD ($). Be aware of GOBLINS (@)! If you are hurt, drink POTIONS (+) to heal. You may find TRAPS (_) which you can disable easily to make for exploring.")
+        print("Each turn, you interact with a cell in the self.board PLUS every other cell of the same type horizontally or vertically connected with it.")
+        print("Those cells are removed from the board, the remaining cells fall to fill the blank spaces and new cells appear to fill the board.")
+        print("")
         
-    print(
- '''
-  _____    _____     _____       _       
- |  __ \  |  __ \   / ____|     (_)      
- | |__) | | |__) | | |  __       _  ___  
- |  _  /  |  ___/  | | |_ |     | |/ _ \ 
- | | \ \  | |      | |__| |  _  | | (_) |
- |_|  \_\ |_|       \_____| (_) |_|\___/
- By RACSO. http://racso.co\n\n
- ''')
-    print("You're exploring a dangerous dungeon to collect GOLD ($). Be aware of GOBLINS (@)! If you are hurt, drink POTIONS (+) to heal. You may find TRAPS (_) which you can disable easily to make for exploring.")
-    print("Each turn, you interact with a cell in the board PLUS every other cell of the same type horizontally or vertically connected with it.")
-    print("Those cells are removed from the board, the remaining cells fall to fill the blank spaces and new cells appear to fill the board.")
-    print("")
-    print("GRAPHIC MODES:")
-    print("1. Basic. Ideal for new players.")
-    print("2. Minimum. Ideal for veterans for faster gameplay.")
-    mode = read("Select mode: ","Invalid mode",1,2)
-    
-    if mode==2:
-        GOBLIN, POTION, MONEY, TRAP, EMPTY = "@", "+", "$", "_", ""
+        print("GRAPHIC MODES:")
+        print("1. Basic. Ideal for new players.")
+        print("2. Minimum. Ideal for veterans for faster gameplay.")
+        self.mode = self.read("Select mode: ","Invalid mode",1,3)
 
-    items = [GOBLIN, POTION, MONEY, TRAP]
-    
-    print("\n"*100)
-    
-    size = 5
-    board = [[EMPTY]*size for i in range(size)]
-    fillBoard()
+        if self.mode==1:
+            self.GOBLIN, self.POTION, self.MONEY, self.TRAP, self.EMPTY = "Goblin", "Potion", " Gold ", " Trap ", "      "
+        else:
+            self.GOBLIN, self.POTION, self.MONEY, self.TRAP, self.EMPTY = "@", "+", "$", "_", ""
+        self.items = [self.GOBLIN, self.POTION, self.MONEY, self.TRAP]
 
-    gold = 0
-    hp = 15
-    turn = 1
+        print("BOARD SIZE & DIFFICULTY:\n1. Small (5x5, easy, recommended for learning)\n2. Medium (7x7, normal)\n3. Big (10x10, hard)\n4. Fantastic (14x14, very hard)")
+        self.size = self.read("Select size: ","Invalid size",1,4)
 
-    while True:
-        printBoard()
-        print("")
-        printStats()
-        print("")
-        command = read("Cell number to interact with: ", "Invalid command.", 0, size*size-1)
+        self.hp = 30 * self.size**2
+        self.size = 4 + (self.size*(self.size+1))//2
         
-        print("\n"*3)
-        print("="*10)
-        print("Your selection: cell %s" % command)
-        taken = list(takenCells(command))
-        processCells(taken)
-        removeFromBoard(taken)
-        receiveDamage()
-        if hp<=0:
-            print("YOU DIED!\n")
-            print("You collected " + str(gold) + " GOLD in " + str(turn) + " turns.")
-            return
-        print("")
-        print("Updating the board (removing your cells, letting cells fall to fill the empty spaces, filling the board with new cells)...")
-        print("")
-        fillBoard()
-        turn += 1
+        self.board = [[self.EMPTY]*self.size for i in range(self.size)]
+        self.fillBoard()
 
-Game()
+        self.gold = 0
+        self.turn = 1
+
+        print("\n"*100)
+
+        while True:
+            self.printBoard()
+            print("")
+            self.printStats()
+            print("")
+            command = self.read("Cell number to interact with: ", "Invalid command.", 0, self.size*self.size-1)
+            print("\n"*3)
+            print("="*10)
+            print("Your selection: cell %s" % command)
+            taken = list(self.takenCells(command))
+            self.processCells(taken)
+            self.removeFromBoard(taken)
+            self.receiveDamage()
+            if self.hp<=0:
+                print("YOU DIED!\n")
+                print("You collected " + str(self.gold) + " gold in " + str(self.turn) + " turns.")
+                return
+            print("")
+            print("Updating the board (removing your cells, letting cells fall to fill the empty spaces, filling the board with new cells)...")
+            print("")
+            self.fillBoard()
+            self.turn += 1
+
+Game().run()
 
