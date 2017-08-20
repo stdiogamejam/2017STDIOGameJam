c8443ab633b9fe5e52e7adb4893679fdc18d05b9
Omar Rizwan <omar.rizwan@gmail.com>
2017-08-20 13:03:17 -0700

    Menu and machine logic.

--- osnr/PaperclipMaximizer.py
+++ osnr/PaperclipMaximizer.py
@@ -1,10 +1,12 @@
-from collections import namedtuple
+from __future__ import print_function
+from collections import namedtuple, defaultdict
 import time
 
-total_grams_converted = 0
-current_level = 'earth'
+playerPaperclips = 0
+currentLevel = 'earth'
 levels = {'earth': 5.972 * 10**27, 'mars': 6.39*10**26, 'jupiter': 1.898*10**30}
-grams_for_spaceship = 531000000
+playerMachines = defaultdict(int) 
+gramsForSpaceship = 531000000
 capacity = 10
 
 # In paperclips / turns.
@@ -25,25 +27,89 @@ def introPage():
         time.sleep(0.1)
         print ("...")
 
-def turn():
-    global total_grams_converted
+def last_turn_report():
+    print ("Last turn, I converted {} paperclips myself.".format(10)) # FIXME: Get number.
 
-    print ("I have constructed {} paperclips so far.".format(total_grams_converted))
+    if len(playerMachines) > 0:
+        print ("I have {} machines working for me, too:".format(len(playerMachines)))
+        for machine, count in playerMachines.items():
+            print ("- {} {} machines converted {} paperclips.".format(
+                count, machine.name, machine.output * count))
+
+    print ("In total, {} paperclips were brought into the world last turn.".format(10)) # FIXME: Get number
     print ()
 
+def overall_report():
+    print ("I have constructed {} paperclips so far.".format(playerPaperclips))
+    print ()
+
+# choice : (description, data)
+def input_menu(choices):
+    while True:
+        for idx, choice in enumerate(choices):
+            print ("{}. {}".format(idx + 1, choice[0]))
+
+        print("> ", end='')
+        try:
+            choice_idx = int(input()) - 1
+
+            # If the user picked a valid choice, accept it.
+            if choice_idx < len(choices):
+                chosen_choice = choices[choice_idx]
+                break
+        except ValueError:
+            pass # Pass to the error message below.
+
+        # Otherwise, give an error and let them pick again.
+        print ("Not a valid option.") # TODO: Better message.
+
+    return chosen_choice[1]
+
+def buy_machine(machine):
+    playerMachines[machine] += 1
+
+
+def convertPaperclips():
+    global playerPaperclips
+    for machine, count in playerMachines.items():
+        playerPaperclips += count * machine.output
+        
+def turn():
+    global playerPaperclips
+
+    last_turn_report()
+
+    overall_report()
+    
     print ("What should I do this turn?")
-    print ("1. Convert {} paperclips".format(capacity))
-    print ("2. Build an automatic paperclip converter")
-    print ("3. Upgrade myself")
-    choice = input()
-    if choice == 1:
-        total_grams_converted += capacity
-    elif choice == 2:
+    turn_choices = [
+        ("Convert {} paperclips".format(capacity), 1),
+        ("Build an automatic paperclip converter", 2),
+        ("Upgrade myself", 3)
+    ]
+    turn_choice = input_menu(turn_choices)
+    if turn_choice == 1:
+        playerPaperclips += capacity
+
+    elif turn_choice == 2:
         print ("Choose a model....")
+
+        machine_choices = []
         for machine in machines:
-            print ("{} {} machines".format(capacity / machine.cost, machine.name))
-    elif choice == 3:
+            if not (capacity / machine.cost) < 1:
+                machine_choices.append((
+                    "{} {} machines".format(capacity // machine.cost, machine.name),
+                    machine
+                ))
+
+        chosen_machine = input_menu(machine_choices)
+        buy_machine(chosen_machine)
+
+    elif turn_choice == 3:
         pass
+    
+    convertPaperclips()
+    print ()
 
 def main():
     introPage()
