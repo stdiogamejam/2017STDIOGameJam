a1fbac8ec3b68e42052037eaa7bdba615b9587ce
Darius Bacon <darius@wry.me>
2017-08-20 14:09:45 -0700

    some cleanup and slightly friendlier interaction

--- darius/cryptopuzzle/cryptopuzzle.py
+++ darius/cryptopuzzle/cryptopuzzle.py
@@ -13,43 +13,8 @@ def get_cryptogram():
     while True:
         line = raw_input()
         if line.strip() == '.': break
-        lines.append(line.lower())
+        lines.append(clean(line.lower()))
     return '\n'.join(lines)
-
-alphabet = string.ascii_lowercase
-
-def play_puzzle(cryptogram):
-    code = ''.join(c for c in cryptogram if c.isalpha())
-    if not code:
-        print "No text to decrypt!"
-        return
-    decoder = {c: ' ' for c in set(code)}
-    lines = map(clean, cryptogram.splitlines())
-
-    while True:
-        used = set(decoder.values()) - set(' ')
-        letters_left = ''.join(' ' if c in used else c for c in alphabet)
-
-        for line in lines:
-            print
-            print ''.join(decoder.get(c, c) for c in line)
-            print ''.join(' -'[c.isalpha()] for c in line)
-            print line
-        print
-        print "Free: %s\n" % letters_left
-
-        cmd = raw_input("Enter a substitution like 'crypt/plain': ")
-        if not cmd.strip():
-            quitting = raw_input("Do you want to quit? ('y' for yes) ")
-            if 'y' in quitting.lower():
-                return
-        if '/' not in cmd:
-            print "Enter something like 'abc/xyz'"
-        else:
-            pattern, replacement = cmd.split('/', 2)
-            for p, r in itertools.izip_longest(pattern, replacement, fillvalue=' '):
-                if p.isalpha():
-                    decoder[p] = r
 
 def clean(s):
     "Expand tabs; blank out other control characters."
@@ -64,6 +29,42 @@ def clean(s):
         else:
             r += c
     return r
+
+alphabet = string.ascii_lowercase
+
+def play_puzzle(cryptogram):
+    code = ''.join(c for c in cryptogram if c.isalpha())
+    if not code:
+        print "No text to decrypt!"
+        return
+    decoder = {c: ' ' for c in set(code)}
+    lines = cryptogram.splitlines()
+
+    while True:
+        used = set(decoder.values()) - set(' ')
+        letters_left = ''.join(' ' if c in used else c for c in alphabet)
+
+        for line in lines:
+            print
+            print ''.join(decoder.get(c, c) for c in line)
+            print ''.join(' -'[c.isalpha()] for c in line)
+            print line
+        print
+        print "Free: %s\n" % letters_left
+
+        while True:
+            command = raw_input("Enter a substitution like 'crypt/plain': ")
+            if not command.strip():
+                quitting = raw_input("Do you want to quit? ('y' for yes) ")
+                if 'y' in quitting.lower():
+                    return
+            if '/' in command: break
+            print "Enter something like 'abc/xyz', or just hit Enter if you want to quit."
+
+        pattern, replacement = command.split('/', 2)
+        for p, r in itertools.izip_longest(pattern, replacement, fillvalue=' '):
+            if p.isalpha():
+                decoder[p] = r
 
 if __name__ == '__main__':
     main()
